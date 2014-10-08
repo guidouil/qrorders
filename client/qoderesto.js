1461e3d6f494daebd9516ed6c18da34a4167734a
Guidouil <guidouil@gmail.com>
2014-10-08 18:12:47 +0200

    adding i18n and accounts-entry packages

--- client/qoderesto.js
+++ client/qoderesto.js
@@ -5,3 +5,37 @@ Accounts.ui.config({
 Template.layout.rendered = function () {
   Meteor.call('user_profile_name');
 };
+
+getUserLanguage = function () {
+  var language = window.navigator.userLanguage || window.navigator.language;
+  if (language.length > 2) {
+    language = language.substring(0,2).toLowerCase();
+  };
+  Session.set('language', language);
+  //return language;
+};
+
+
+Meteor.startup(function () {
+  getUserLanguage();
+
+  AccountsEntry.config({
+    homeRoute: '/',                 // mandatory - path to redirect to after sign-out
+    dashboardRoute: '/dash',      // mandatory - path to redirect to after successful sign-in
+    wrapLinks: true,
+    passwordSignupFields: 'USERNAME_AND_EMAIL',
+    language: Session.get('language'),
+    showOtherLoginServices: true//,     // Set to false to hide oauth login buttons on the signin/signup pages. Useful if you are using
+  });
+
+  Session.set("showLoadingIndicator", true);
+
+  TAPi18n.setLanguage(Session.get('language'))
+    .done(function () {
+      Session.set("showLoadingIndicator", false);
+    })
+    .fail(function (error_message) {
+      // Handle the situation
+      console.log(error_message);
+    });
+});
