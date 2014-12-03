c8b7213b38d683f117050b4997e3139bf41bb72a
Guidouil <guidouil@gmail.com>
2014-12-03 23:33:36 +0100

    log removin'

--- client/views/search/search.js
+++ client/views/search/search.js
@@ -1,13 +1,11 @@
 Template.search.helpers({
   places: function () {
     var searched = decodeURI(Router.current().params.searched);
-    console.log(Session.get('here'));
     if (searched === 'undefined' && Session.get('here') !== 'undefined') {
       var here = Session.get('here');
       return Places.find({loc: { $near: { $geometry: { type:"Point", coordinates:[here.lon, here.lat]}}, $maxDistance: 3000} }).fetch();
     }
     if (searched !== 'undefined') {
-      console.log('yaya');
       return Places.find({$or: [{placename: { $regex: searched, $options: 'i' }}, {phone: { $regex: searched, $options: 'i' }}, {street: { $regex: searched, $options: 'i' }}, {town: { $regex: searched, $options: 'i' }}, {zip: { $regex: searched, $options: 'i' }}, {placedesc: { $regex: searched, $options: 'i' }}, {PlacesTags: { $regex: searched, $options: 'i' }}, {code: { $regex: searched, $options: 'i' }}]}).fetch();
     }
   }
