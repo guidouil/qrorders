d43bd24df5df23fa550724f7cc2fe1240b580d6a
Guidouil <guidouil@gmail.com>
2014-12-28 16:44:17 +0100

    Payments rockin'

--- client/views/places/payments/payments.js
+++ client/views/places/payments/payments.js
@@ -1,7 +1,16 @@
 Template.paymentsPlace.helpers({
   payments: function () {
     if (_.contains(this.owner, Meteor.userId())) {
-      return Payments.find().fetch();
+      return Payments.find({},{sort: {date: -1}}).fetch();
     }
+  },
+  bigTotal: function () {
+    var bigTotal = 0;
+    if (_.contains(this.owner, Meteor.userId())) {
+      Payments.find({}).map(function(doc) {
+        bigTotal += doc.total;
+      });
+    }
+    return bigTotal;
   }
 });
