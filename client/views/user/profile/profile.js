0a68f47f5f9a9feb4d69342c56e9adaae83f112d
Guidouil <guidouil@gmail.com>
2014-12-30 21:15:32 +0100

    Email showin'

--- client/views/user/profile/profile.js
+++ client/views/user/profile/profile.js
@@ -58,5 +58,17 @@ Template.profile.helpers({
   placeName: function (placeId) {
     var place = Places.findOne({_id: placeId});
     return place.placename + ' - ' + place.town;
+  },
+  contactEmail: function () {
+    var user = Meteor.user();
+    if (user.emails && user.emails.length)
+      return user.emails[0].address;
+    if (user.services && user.services.facebook && user.services.facebook.email)
+      return user.services.facebook.email;
+    if (user.services && user.services.google && user.services.google.email)
+      return user.services.google.email;
+    if (user.services && user.services.twitter && user.services.twitter.email)
+      return user.services.twitter.email;
+    return null;
   }
 });
