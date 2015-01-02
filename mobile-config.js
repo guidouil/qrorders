3f74275928f182dc6ed0a2bb1a2a723e9472c5e5
Guidouil <guidouil@gmail.com>
2015-01-02 15:50:51 +0100

    iphone building

--- mobile-config.js
+++ mobile-config.js
@@ -9,10 +9,10 @@ App.info({
 
 App.icons({
   // iOS
-  // 'iphone': 'resources/icons/icon-60x60.png',
-  // 'iphone_2x': 'resources/icons/icon-60x60@2x.png',
-  // 'ipad': 'resources/icons/icon-72x72.png',
-  // 'ipad_2x': 'resources/icons/icon-72x72@2x.png',
+  'iphone': 'resources/icons/icon-60x60.png',
+  'iphone_2x': 'resources/icons/icon-60x60@2x.png',
+  'ipad': 'resources/icons/icon-72x72.png',
+  'ipad_2x': 'resources/icons/icon-72x72@2x.png',
 
   // Android
   'android_ldpi': 'resources/icons/icon-36x36.png',
@@ -25,11 +25,11 @@ App.launchScreens({
   // iOS
   // 'iphone': 'resources/splash/splash-320x480.png',
   // 'iphone_2x': 'resources/splash/splash-320x480@2x.png',
-  // 'iphone5': 'resources/splash/splash-320x568@2x.png',
-  // 'ipad_portrait': 'resources/splash/splash-768x1024.png',
-  // 'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-  // 'ipad_landscape': 'resources/splash/splash-1024x768.png',
-  // 'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+  'iphone5': 'resources/splash/splash-320x568@2x.png',
+  'ipad_portrait': 'resources/splash/splash-768x1024.png',
+  'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+  'ipad_landscape': 'resources/splash/splash-1024x768.png',
+  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
   // Android
   'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
