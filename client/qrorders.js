ba4f54e3f0bc4dfd573d8aaee15959fbb804096d
Guidouil <guidouil@gmail.com>
2015-01-15 17:31:28 +0100

    App cachin'

--- client/qrorders.js
+++ client/qrorders.js
@@ -23,6 +23,8 @@ getUserLanguage = function () {
 };
 
 Meteor.startup(function () {
+  Meteor.AppCache.config({onlineOnly: ['/resources/']});
+
   Accounts.ui.config({
     passwordSignupFields: 'EMAIL_ONLY'
   });
